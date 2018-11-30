f39396f7ac4bea0bd3debb800bf089905cfc73ca
Max Kavun <maxkavun@outlook.com>
2018-11-30 10:10:20 -0800

    Test errors checker Jenkins

--- polls/views.py
+++ polls/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-	return HttpResponse("My new text for Jenkins test")
+	return HttpRsponse("My new text for Jenkins test")
